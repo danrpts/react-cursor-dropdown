48a4da75e4b77c45f49f6b3284c41ba3a82c10a1
Daniel <danrpts@gmail.com>
2018-10-07 19:51:40 -0700

    Changing API: HOC for parent and dropdowns passed as children

--- src/index.js
+++ src/index.js
@@ -4,11 +4,112 @@ import getCaretCoordinates from "textarea-caret";
 
 import styles from "./styles.css";
 
+const KeywordContext = React.createContext("");
+
+function WithCaretDropdown(WrappedComponent) {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        caret: {
+          top: 0,
+          left: 0
+        },
+        keyword: "",
+        visible: true
+      };
+
+      this.updateState = el => {
+        const keyword = getCaretWord(el).word;
+        const caret = getCaretCoordinates(el, el.selectionStart);
+        const top = el.offsetTop + caret.top + caret.height - el.scrollTop;
+        const left = el.offsetLeft + caret.left - el.scrollLeft;
+
+        const inYBounds =
+          el.offsetTop <= top && top <= el.offsetTop + el.offsetHeight;
+        const inXBounds =
+          el.offsetLeft <= left && left <= el.offsetLeft + el.offsetWidth;
+
+        this.setState({
+          caret: {
+            top,
+            left
+          },
+          keyword,
+          display: inYBounds && inXBounds ? "block" : "none"
+        });
+      };
+
+      this.onScroll = e => {
+        this.updateState(e.target);
+        this.props.onScroll && this.props.onScroll(e);
+      };
+      this.onSelect = e => {
+        this.updateState(e.target);
+        this.props.onSelect && this.props.onSelect(e);
+      };
+      this.onChange = e => {
+        this.updateState(e.target);
+        this.props.onChange && this.props.onChange(e);
+      };
+      this.onClick = e => {
+        this.updateState(e.target);
+        this.props.onClick && this.props.onClick(e);
+      };
+    }
+
+    render() {
+      const {
+        children,
+        onScroll, //ignore
+        onSelect, //ignore
+        onChange, //ignore
+        onClick, //ignore
+        ...rest
+      } = this.props;
+      return (
+        <div className={styles.caretDropdownContainer}>
+          <WrappedComponent
+            onScroll={this.onScroll}
+            onSelect={this.onSelect}
+            onChange={this.onChange}
+            onClick={this.onClick}
+            {...rest}
+          />
+          <div
+            className={styles.caretDropdown}
+            style={{
+              display: this.state.display,
+              top: this.state.caret.top,
+              left: this.state.caret.left
+            }}
+          >
+            <KeywordContext.Provider value={this.state.keyword}>
+              {children}
+            </KeywordContext.Provider>
+          </div>
+        </div>
+      );
+    }
+  };
+}
+
+function CaretDropdown({ pattern, component }) {
+  return (
+    <KeywordContext.Consumer>
+      {keyword => {
+        const match = keyword.match(pattern);
+        return match && React.createElement(component, { match });
+      }}
+    </KeywordContext.Consumer>
+  );
+}
+
+// Helper to get current selected word, or the word under the caret
 function getCaretWord(el) {
   let start = el.selectionStart;
   let end = el.selectionEnd;
   let word = el.value.substring(start, end);
-  // if there is no selection, grab the word under the caret
   if (start === end) {
     let wordLeft = el.value
       .substring(0, start)
@@ -23,97 +124,4 @@ function getCaretWord(el) {
   return { word, start, end };
 }
 
-const WithCaretDropdown = WrappedInputComponent => (
-  ...WrappedListComponents
-) => {
-  return class CaretDropdown extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        caret: {
-          top: 0,
-          left: 0
-        },
-        keyword: "",
-        show: false
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleClick = this.handleClick.bind(this);
-      this.handleSelect = this.handleSelect.bind(this);
-      this.handleScroll = this.handleScroll.bind(this);
-    }
-
-    handleChange(e) {
-      const { onChange } = this.props;
-      this.updateState(e.target);
-      onChange && onChange(e);
-    }
-    handleClick(e) {
-      const { onClick } = this.props;
-      this.updateState(e.target);
-      onClick && onClick(e);
-    }
-    handleSelect(e) {
-      const { onSelect } = this.props;
-      this.updateState(e.target);
-      onSelect && onSelect(e);
-    }
-    handleScroll(e) {
-      const { onScroll } = this.props;
-      this.updateState(e.target);
-      onScroll && onScroll(e);
-    }
-
-    updateState(el) {
-      const keyword = getCaretWord(el).word;
-      const caret = getCaretCoordinates(el, el.selectionStart);
-      const top = el.offsetTop + caret.top + caret.height - el.scrollTop;
-      const left = el.offsetLeft + caret.left - el.scrollLeft;
-
-      const inYBounds =
-        el.offsetTop <= top && top <= el.offsetTop + el.offsetHeight;
-      const inXBounds =
-        el.offsetLeft <= left && left <= el.offsetLeft + el.offsetWidth;
-
-      this.setState({
-        caret: {
-          top,
-          left
-        },
-        keyword,
-        show: inYBounds && inXBounds
-      });
-    }
-
-    render() {
-      return (
-        <div className={styles.caretDropdownContainer}>
-          <WrappedInputComponent
-            {...this.props}
-            onChange={this.handleChange}
-            onClick={this.handleClick} // resize event
-            onSelect={this.handleSelect}
-            onScroll={this.handleScroll}
-          />
-          <div
-            className={styles.caretDropdown}
-            style={{
-              top: this.state.caret.top,
-              left: this.state.caret.left
-            }}
-          >
-            {WrappedListComponents.map((List, i) => (
-              <List
-                key={i}
-                keyword={this.state.keyword}
-                visible={this.state.show}
-              />
-            ))}
-          </div>
-        </div>
-      );
-    }
-  };
-};
-
-export default WithCaretDropdown;
+export { WithCaretDropdown, CaretDropdown };
