53bed37886e507b8faeeecefada9e65872ce3275
Daniel <danrpts@gmail.com>
2018-10-12 01:47:31 -0700

    Polished up example

--- example/src/App.js
+++ example/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { WithCursorDropdown, CursorDropdown } from "react-cursor-dropdown";
 import EmojiList from "./EmojiList.js";
 import MentionList from "./MentionList.js";
-import { Container, Row, Col, Input } from "reactstrap";
+import { Container, Row, Col, Input, FormText } from "reactstrap";
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -14,10 +14,10 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      value: "",
+      value: ":",
       selection: {
-        start: 0,
-        end: 0
+        start: 1,
+        end: 1
       }
     };
 
@@ -39,14 +39,47 @@ export default class App extends Component {
       <Container className="my-5">
         <Row className="justify-content-center">
           <Col sm="12" md="10" lg="8">
+            <div className="text-center mb-5">
+              <h1 className="display-4">react-cursor-dropdown</h1>
+              <p className="lead">
+                A React HOC for adding cursor dropdown menus to textareas and
+                inputs.
+              </p>
+              <p>
+                <a
+                  className="github-button"
+                  href="https://github.com/danrpts/react-cursor-dropdown"
+                  data-icon="octicon-star"
+                  data-size="large"
+                  aria-label="Star danrpts/react-cursor-dropdown on GitHub"
+                >
+                  Star
+                </a>
+                <span className="ml-2">
+                  <a
+                    className="github-button"
+                    href="https://github.com/danrpts/react-cursor-dropdown/fork"
+                    data-icon="octicon-repo-forked"
+                    data-size="large"
+                    aria-label="Fork danrpts/react-cursor-dropdown on GitHub"
+                  >
+                    Fork
+                  </a>
+                </span>
+              </p>
+            </div>
+            <FormText color="muted">
+              Type to filter the emoji list, use the up/down arrows to move the
+              highlight, and enter/tab to select.
+            </FormText>
             <InputCursorDropdown
               rows="3"
               value={this.state.value}
               selection={this.state.selection}
               onChange={this.onChange}
+              focusOnMount
             >
               <CursorDropdown pattern={/^:([\w+-]*)$/} component={EmojiList} />
-              <CursorDropdown pattern={/^@(\w*)$/} component={MentionList} />
             </InputCursorDropdown>
           </Col>
         </Row>
